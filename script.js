c2311943e0648dafcdf911efc42233710f2d354f
James Lawrence <jamesblawrence2007@gmail.com>
2024-10-16 07:45:43 -0600

    reorganized data into arrays to redo api calling

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-import apidata from "./apidata.js";
+const thanksgivingPointData = {
+    yardageArray: [],
+    parArray: [],
+    handicapArray: []
+}
+const foxHollowData = {
+    yardageArray: [],
+    parArray: [],
+    handicapArray: []
+}
+const spanishOaksData = {
+    yardageArray: [],
+    parArray: [],
+    handicapArray: []
+}
+const yardageInputs = [document.getElementById("yardage-one"), document.getElementById("yardage-two"), document.getElementById("yardage-three"), document.getElementById("yardage-four"), document.getElementById("yardage-five"), document.getElementById("yardage-six"), document.getElementById("yardage-seven"), document.getElementById("yardage-eight"), document.getElementById("yardage-nine"), document.getElementById("yardage-ten"), document.getElementById("yardage-eleven"), document.getElementById("yardage-twelve"), document.getElementById("yardage-thirteen"), document.getElementById("yardage-fourteen"), document.getElementById("yardage-fifteen"), document.getElementById("yardage-sixteen"), document.getElementById("yardage-seventeen"), document.getElementById("yardage-eighteen")];
+const parInputs = [document.getElementById("par-one"), document.getElementById("par-two"), document.getElementById("par-three"), document.getElementById("par-four"), document.getElementById("par-five"), document.getElementById("par-six"), document.getElementById("par-seven"), document.getElementById("par-eight"), document.getElementById("par-nine"), document.getElementById("par-ten"), document.getElementById("par-eleven"), document.getElementById("par-twelve"), document.getElementById("par-thirteen"), document.getElementById("par-fourteen"), document.getElementById("par-fifteen"), document.getElementById("par-sixteen"), document.getElementById("par-seventeen"), document.getElementById("par-eighteen")];
+const handicapInputs = [document.getElementById("handicap-one"), document.getElementById("handicap-two"), document.getElementById("handicap-three"), document.getElementById("handicap-four"), document.getElementById("handicap-five"), document.getElementById("handicap-six"), document.getElementById("handicap-seven"), document.getElementById("handicap-eight"), document.getElementById("handicap-nine"), document.getElementById("handicap-ten"), document.getElementById("handicap-eleven"), document.getElementById("handicap-twelve"), document.getElementById("handicap-thirteen"), document.getElementById("handicap-fourteen"), document.getElementById("handicap-fifteen"), document.getElementById("handicap-sixteen"), document.getElementById("handicap-seventeen"), document.getElementById("handicap-eighteen")];
